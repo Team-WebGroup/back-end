ae5e8193124dfe105bcda7f49039fc2dcc512d81
ncsmatias <nicolasmarcelo30@gmail.com>
2021-08-01 12:59:29 -0300

    feat: create routes session and user

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
+const UserController = require('./app/controllers/userController');
+const SessionController = require('./app/controllers/sessionController');
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ msg: 'Hello World' });
-});
+routes.post('/session', SessionController.store);
 
+routes.post('/user', UserController.store);
+routes.get('/user', UserController.get);
+routes.put('/user/:id', UserController.update);
+routes.delete('/user/:id', UserController.delete);
 
 module.exports = routes;
