38fdb0274de78c79eb55a3b8ff25b52236add629
ncsmatias <nicolasmarcelo30@gmail.com>
2021-08-05 20:45:36 -0300

    feat: import auth middleware

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,19 @@ const UserController = require('./app/controllers/userController');
 const SessionController = require('./app/controllers/sessionController');
 const ProductsController = require('./app/controllers/procutsController');
 
+const AuthMiddleware = require('./app/middlewares/auth').auth;
+
 const routes = Router();
 
 routes.post('/session', SessionController.store);
 
+
+
 routes.post('/user', UserController.store);
 routes.get('/user', UserController.get);
-routes.put('/user/:id', UserController.update);
+routes.put('/user/:id',AuthMiddleware, UserController.update);
 routes.delete('/user/:id', UserController.delete);
 
-routes.get('/procuts', );
+routes.get('/procuts', ); 
 
 module.exports = routes;
