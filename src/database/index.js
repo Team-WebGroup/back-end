4aead0451cbd4a7c7a9055bdb372ac500029f633
ncsmatias <nicolasmarcelo30@gmail.com>
2021-08-05 21:58:52 -0300

    feat: connection address

--- src/database/index.js
+++ src/database/index.js
@@ -2,11 +2,12 @@ const sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
 const User = require('../app/models/User');
+const Address = require('../app/models/Address');
 
 
 const connection = new sequelize(dbConfig);
 
-const models = [User];
+const models = [User, Address];
 
 class Database {
   constructor() {
@@ -17,6 +18,8 @@ class Database {
     this.connection = new sequelize(dbConfig);
 
     models.map(model => model.init(this.connection));
+
+    Address.associate(this.connection.models);
   }
 }
 
